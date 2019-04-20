a92e46ae44d2c7e7a3ee682d3fde480887b21842
Batin Eryilmaz <batineryilmaz@icloud.com>
2019-04-20 12:40:25 +0300

    quick fix

--- src/main.js
+++ src/main.js
@@ -4,18 +4,9 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import PouchDB from 'pouchdb'
 import TablerVue from '../dist/vue-tabler'
 
 Vue.use(TablerVue)
-
-
-Vue.use(require('vue-pouch'), {
-  pouch: PouchDB,    // optional if `PouchDB` is available on the global object
-  defaultDB: 'mac_scores',  // this is used as a default connect/disconnect database
-  optionDB: {}, // this is used to include a custom fetch() method (see TypeScript example)
-  debug: '*' // optional - See `https://pouchdb.com/api.html#debug_mode` for valid settings (will be a separate Plugin in PouchDB 7.0)
-})
 
 Vue.config.productionTip = false
 
