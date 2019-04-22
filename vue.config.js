ee9f01bd9b909d3fe7475102d5c7270e1e4682a8
Batin Eryilmaz <batineryilmaz@icloud.com>
2019-04-22 16:21:54 +0300

    merge

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
-// vue.config.js
-module.exports = {
-  publicPath: '<https://github.com/batin/MacScores>'
-} 
-=======
  // vue.config.js
 module.exports = {
   publicPath: `<goMacScores>`
 }
->>>>>>> deployv2
